e2f413436cb620a87f71ca87d916a39d91308a34
Miguel <miguelnov2006@live.fr>
2019-02-28 23:15:35 -0500

    baseline

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
+import Slider from './components/slider/slider'
 
 
 class App extends Component {
   render() {
     return (
       <div >
-        Hello
+
+        <Slider/>
       </div>
     );
   }
---
+++ src/components/slider/slider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './slider.module.css'
+
+const Slider = () => {
+
+    return (
+        <div className={classes.Slider}>Hello</div>
+    )
+}
+
+export default Slider
