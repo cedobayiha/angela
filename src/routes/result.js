2a3b75229846cf3cd709a754682281c3c277f1ba
Miguel <miguelnov2006@live.fr>
2019-03-01 06:24:43 -0500

    clear conflict

--- src/routes/result.js
+++ src/routes/result.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import Subnav from '../components/result/subnav'
-<<<<<<< Updated upstream
+
 import ImagesHeader from '../components/result/imagesHeader'
-=======
+
 import Email from '../components/Email'
 import Navv from '../components/navv'
 
 
->>>>>>> Stashed changes
 
 const Result = () => (
     <div>
         <Subnav/>
-<<<<<<< Updated upstream
         <ImagesHeader/>
-=======
         <Navv />
         <Email />
 
         <div className="space20"/>
->>>>>>> Stashed changes
     </div>
 )
 
